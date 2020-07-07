44d9aa66276e22610e505dc92d70f62b0cd61345
ZhaoMuwei <zhaomuwei@baidu.com>
2020-07-07 18:02:26 +0800

    Updates

---
+++ precache-manifest.1a65899219ef1ef1cb7cb4878c14ca5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae404eb1d3b62b3d7e9e97797e9c870e",
+    "url": "/baidu-viz-iframe-demo/index.html"
+  },
+  {
+    "revision": "169994b73b3b16fb9724",
+    "url": "/baidu-viz-iframe-demo/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "e869970427db10d1c6cf",
+    "url": "/baidu-viz-iframe-demo/static/css/main.2d51ff4f.chunk.css"
+  },
+  {
+    "revision": "169994b73b3b16fb9724",
+    "url": "/baidu-viz-iframe-demo/static/js/2.b72c1150.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/baidu-viz-iframe-demo/static/js/2.b72c1150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e869970427db10d1c6cf",
+    "url": "/baidu-viz-iframe-demo/static/js/main.5fd23194.chunk.js"
+  },
+  {
+    "revision": "144d512ef312c82702ac",
+    "url": "/baidu-viz-iframe-demo/static/js/runtime-main.0bdb1d39.js"
+  }
+]);
